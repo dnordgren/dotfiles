9dd6c91311e58ee293eda300635fb6e57c3ead00
Derek Nordgren <dnordgren@users.noreply.github.com>
2017-07-31 17:26:42 -0500

    Create Microsoft.PowerShell_profile.ps1

---
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,8 @@
+# Load posh-git example profile
+. 'C:\Users\derek.nordgren\Documents\WindowsPowerShell\Modules\posh-git\profile.example.ps1'
+
+# default start path
+Set-Location "C:\"
+
+# code alias
+Set-Alias code code-insiders
