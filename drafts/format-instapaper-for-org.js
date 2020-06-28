19ca8e69f0fb98a8fb3de5daa790111f9523c132
Derek Nordgren <derek.nordgren@hudl.com>
2020-06-28 12:46:37 +0100

    Update drafts script for DEVONthink slug.

--- drafts/format-instapaper-for-org.js
+++ drafts/format-instapaper-for-org.js
@@ -13,19 +13,31 @@ const uuid = () => {
   });
 };
 
-const d = draft.createdDate;
+const d = draft.createdAt;
 
-const ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d);
-const mo = new Intl.DateTimeFormat('en', { month: '2-digit' }).format(d);
-const da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(d);
+const yearLong  = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d);
+const yearShort = new Intl.DateTimeFormat('en', { year: '2-digit' }).format(d);
+const month     = new Intl.DateTimeFormat('en', { month: '2-digit' }).format(d);
+const day       = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(d);
+const weekday   = new Intl.DateTimeFormat('en', { weekday: 'short' }).format(d);
+
+const markdownTitle = draft.title;
+const titleRegex = /\[(?<title>.*)\]/;
+const urlRegex   = /\((?<url>.*)\)/;
+
+const titleMatch = markdownTitle.match(titleRegex);
+const urlMatch = markdownTitle.match(urlRegex);
+
+const title = titleMatch && titleMatch.groups ? titleMatch.groups.title : '';
+const url = urlMatch && urlMatch.groups ? urlMatch.groups.url : '';
 
 const modeLine = `-*- mode: org -*-\n`;
-const slugLine  = `${ye}-${mo}-${da}.snippet.slug\n`;
-const titleLine = `#+TITLE: ${draft.title}\n`;
-const dateLine  = `#+DATE : ${ye}-${mo}-${da}\n`;
-const urlLine   = `#+URL  : ${draft.title}\n\n`;
+const slugLine  = `${yearShort}${month}${day}-${d.getHours()}${d.getMinutes()} ${title.toLowerCase()}\n`;
+const titleLine = `#+TITLE: ${title}\n`;
+const dateLine  = `#+DATE: ${yearLong}-${month}-${day} ${weekday}\n`;
+const urlLine   = `#+URL: ${url}\n\n`;
 
-const linksDrawer = `:LINKS:\n@:${uuid()}\n:END:\n`;
+const linksDrawer = `:LINKS:\n@:${draft.uuid}\n:END:\n`;
 
 const metadataDrawer = `:METADATA:\nlat : ${draft.createdLatitude}\nlong: ${draft.createdLongitude}\n:END:\n\n`;
 
