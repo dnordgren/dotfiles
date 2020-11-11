29c6db9a70341e31f2c61ce6baf1f53f20ae571c
Derek Nordgren <dnordgren@users.noreply.github.com>
2020-11-11 18:31:04 +0000

    Copy gpg command to clipboard.

--- bash/dtbackup.sh
+++ bash/dtbackup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 date_fmt=$(date +'%y%m%d')
 
-cp -R $HOME/Databases/$1.dtBase2 $HOME/Downloads # don't include trailing / on the database file or cp will copy contents rather than directory itself
+# don't include trailing / on the database file or cp will copy contents rather than directory itself
+cp -R $HOME/Databases/$1.dtBase2 $HOME/Downloads
 
 zip_path=$HOME/Downloads/$1-$date_fmt.dtBase2.tar.gz
 tmp_path=$HOME/Downloads/$1.dtBase2
@@ -10,4 +11,7 @@ tar czf $zip_path $tmp_path
 
 rm -r $tmp_path
 
-echo "gpg --encrypt --sign -r derek@dereknordgren.com $zip_path"
+gpg_cmd="gpg --encrypt --sign -r derek@dereknordgren.com $zip_path"
+
+echo "$gpg_cmd" | pbcopy
+echo "Copied to clipboard: '$gpg_cmd'"
