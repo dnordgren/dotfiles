19e13df64b69103b504bd5a59ecfd9c300c6b88e
Derek Nordgren <derek.nordgren@hudl.com>
2020-12-18 10:27:37 +0000

    Configure org-roam-server.
    Followed
    https://web.archive.org/web/20201218102711/https://www.ianjones.us/org-roam-server/
    based on the GitHub repo instructions.

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -65,27 +65,15 @@
       '(("d" "daily" entry #'org-roam-capture--get-point
          "* %?\n")))
 
-(def-package! tide
-  :init
-  (defun setup-tide-mode ()
-  (interactive)
-  (tide-setup)
-  (flycheck-mode +1)
-  (setq flycheck-check-syntax-automatically '(save mode-enabled))
-  (eldoc-mode +1)
-  (tide-hl-identifier-mode +1)
-  ;; company is an optional dependency. You have to
-  ;; install it separately via package-install
-  ;; `M-x package-install [ret] company`
-  (company-mode +1))
+(require 'org-roam-protocol)
 
-  ;; aligns annotation to the right hand side
-  (setq company-tooltip-align-annotations t)
+(require 'simple-httpd)
+(setq httpd-root "/var/www")
+(httpd-start)
 
-  ;; formats the buffer before saving
-  (add-hook 'before-save-hook 'tide-format-before-save)
-
-  (add-hook 'typescript-mode-hook #'setup-tide-mode))
+(use-package org-roam-server
+  :ensure nil
+  :load-path "~/www/org-roam-server")
 
 ;; Here are some additional functions/macros that could help you configure Doom:
 ;;
