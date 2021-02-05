6f733962b2c41037c590fe99232bed0186aa5a89
Derek Nordgren <derek.nordgren@hudl.com>
2021-02-05 19:04:00 +0000

    Update DEVONthink backup script to work with Inbox.

--- bash/dtbackup.sh
+++ bash/dtbackup.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 date_fmt=$(date +'%y%m%d')
 
+# $1 : database name
+# $2 : database path
+#    : ~/Databases/<db>.dtBase2
+#    : ~/Library/Application\ Support/DEVONthink\ 3/Inbox.dtBase2
+
 # don't include trailing / on the database file or cp will copy contents rather than directory itself
-cp -R $HOME/Databases/$1.dtBase2 $HOME/Downloads
+cp -R "$2" $HOME/Downloads
 
 zip_path=$HOME/Downloads/$1-$date_fmt.dtBase2.tar.gz
 tmp_path=$HOME/Downloads/$1.dtBase2
@@ -15,3 +20,4 @@ gpg_cmd="gpg --encrypt --sign -r derek@dereknordgren.com $zip_path"
 
 echo "$gpg_cmd" | tr -d '\n' | pbcopy
 echo "Copied to clipboard: '$gpg_cmd'"
+
