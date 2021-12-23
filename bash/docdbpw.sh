cfd2fcc90a84b780c9db9be6cae4ae0507210905
Derek Nordgren <derek.nordgren@hudl.com>
2021-12-23 17:32:08 +0000

    Update DocDB AWS to login to Mongo with pwd.

--- bash/docdbpw.sh
+++ bash/docdbpw.sh
@@ -7,9 +7,9 @@ then
     env="thor"
 fi
 
-aws secretsmanager get-secret-value \
-  --secret-id marvel/$env/$1/DocDBCredentials \
-  | jq -r .SecretString | jq -r .Secret | jq -r .Password \
-  | pbcopy
+db_pw=$(aws secretsmanager get-secret-value --secret-id marvel/$env/$1/DocDBCredentials | jq -r .SecretString | jq -r .Secret | jq -r .Password)
 
+echo "$db_pw" | pbcopy
 echo "Copied pw for $1 $env to clipboard"
+
+mongo --host t-$1-docdb.app.thorhudl.com:27018 --username $1 --ssl --sslCAFile ~/.ssh/rds-combined-ca-bundle.pem --sslAllowInvalidCertificates --password $db_pw
