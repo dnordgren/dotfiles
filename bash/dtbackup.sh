2a4e00b99454d3a3924457b084984d12f9b382ca
Derek Nordgren <derek.nordgren@hudl.com>
2022-02-19 15:21:02 +0000

    feat: Update gpg zip naming for DEVONthink backup.

--- bash/dtbackup.sh
+++ bash/dtbackup.sh
@@ -9,14 +9,17 @@ date_fmt=$(date +'%y%m%d')
 # don't include trailing / on the database file or cp will copy contents rather than directory itself
 cp -R "$2" $HOME/Downloads
 
-zip_path=$HOME/Downloads/$1-$date_fmt.dtBase2.tar.gz
+zip_path=$HOME/Downloads/$date_fmt-$1.dtBase2.tar.gz
 tmp_path=$HOME/Downloads/$1.dtBase2
 
 tar czf $zip_path $tmp_path
 
 rm -r $tmp_path
 
-gpg_cmd="gpg --encrypt --sign -r derek@dereknordgren.com $zip_path"
+rm_cmd="rm $zip_path"
+echo "$rm_cmd" | tr -d '\n' | pbcopy
+echo "Copied to clipboard: $rm_cmd"
 
+gpg_cmd="gpg --encrypt --sign -r derek@dereknordgren.com $zip_path"
 echo "$gpg_cmd" | tr -d '\n' | pbcopy
-echo "Copied to clipboard: '$gpg_cmd'"
+echo "Copied to clipboard: $gpg_cmd"
